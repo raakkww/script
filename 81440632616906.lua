827f56294f73267888ab98fff46741cefee1c4a6
raakkww <aditiapribadiansyah5@gmail.com>
2026-01-26 05:47:08 +0700

    Create 81440632616906.lua

---
+++ 81440632616906.lua
@@ -0,0 +1,212 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+while wait() do
+for i,v in pairs(game.Players:GetPlayers()) do
+local args = {
+	"Empyreus"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("PetCageEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"hello"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("DigEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	3
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("SpinPrizeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	20,
+	"bye"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("GemEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Stone"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Iron"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Pumpkicake"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Gold"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Diamond"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Rainbow"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Crystal"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Future"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Ice"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Lava"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Nuke"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Thunder"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Toy"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Unicorn"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Void"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Anime"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Glitch"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Neon"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Pianata"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Angle"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Cake"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Admin"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Plushie"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Emoji"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Firework"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Lunar"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Cosmic"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Solaris"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Star"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Quantum"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Neurospike"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Rocket"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Nebula"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Astro"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Alien"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Mystical"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+task.wait()
+local args = {
+	"Singulaity"
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("BuyPickaxeEvent"):FireServer(unpack(args))
+end
+end
