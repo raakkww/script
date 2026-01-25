cd7ff5b20f60bd9ce0591166aafc7236b261c20d
raakkww <aditiapribadiansyah5@gmail.com>
2026-01-26 06:30:19 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,15 @@
-pcall(function()
-	loadstring(game:HttpGet(string.format("https://raw.githubusercontent.com/raakkww/asdwasdwasdwasdwa/aaaaaaaaaaaaaaaaaaaaa/%s.lua", game.GameId)))()
+task.spawn(function()
+    local ok, err = pcall(function()
+        local placeId = tostring(game.PlaceId)
+        local url = "https://raw.githubusercontent.com/raakkww/asdwasdwasdwasdwa/aaaaaaaaaaaaaaaaaaaaa/" .. placeId .. ".lua"
+
+        local source = game:HttpGet(url)
+        assert(source and #source > 5, "Game script not found")
+
+        loadstring(source)()
+    end)
+
+    if not ok then
+        warn("[LOADER ERROR]:", err)
+    end
 end)
