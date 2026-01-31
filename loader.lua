7465959087365888459c48fc8b7006d025b6250d
raakkww <aditiapribadiansyah5@gmail.com>
2026-02-01 04:11:28 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+task.spawn(function()
+    local ok, err = pcall(function()
+        local placeId = tostring(game.PlaceId)
+        local url = "https://raw.githubusercontent.com/raakkww/script/hehe/" .. placeId .. ".lua"
+
+        local source = game:HttpGet(url)
+        assert(source and #source > 5, "Game script not found")
+
+        loadstring(source)()
+    end)
+
+    if not ok then
+        warn("[LOADER ERROR]:", err)
+    end
+end)
