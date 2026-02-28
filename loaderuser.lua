78b06da9327af8535bf29eb45ff21c24451a4c68
raakkww <aditiapribadiansyah5@gmail.com>
2026-03-01 03:44:53 +0700

    Update loaderuser.lua

--- loaderuser.lua
+++ loaderuser.lua
@@ -1,6 +1,6 @@
 task.spawn(function()
     local ok, src = pcall(function()
-        return game:HttpGet("https://raw.githubusercontent.com/raakkww/script/RoHub/universe_" .. game.GameId, true)
+        return game:HttpGet("https://raw.githubusercontent.com/raakkww/script/RoHub/" .. game.GameId, true)
     end)
     
     if ok and src and #src > 10 and not src:find("<!DOCTYPE") then
