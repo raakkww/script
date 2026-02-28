93bea5c12cb3d557e43b30c031aea93dbb230609
raakkww <aditiapribadiansyah5@gmail.com>
2026-03-01 03:53:42 +0700

    Update loaderdev.lua

--- loaderdev.lua
+++ loaderdev.lua
@@ -1,29 +1,26 @@
 task.spawn(function()
     local ok, err = pcall(function()
-        -- PAKAI GAMEID (Universe ID), BUKAN PLACEID
         local id = tostring(game.GameId)
         local url = "https://raw.githubusercontent.com/raakkww/script/RoHub/" .. id
-        
+
         local src = game:HttpGet(url, true)
         assert(src and #src > 10 and not src:find("<!DOCTYPE"), "Not found")
-        
+
         loadstring(src)()
         print("Loaded: " .. id)
     end)
-    
-    if not ok then 
+
+    if not ok then
         warn("Error: " .. tostring(err))
-        
+
         local universeId = tostring(game.GameId)
         warn("Buat file:" .. universeId)
-        
-        -- AUTO COPY KE CLIPBOARD
+
         pcall(function()
             setclipboard(universeId)
             print("✅ Universe ID copied: " .. universeId)
         end)
-        
-        -- Notifikasi UI
+
         pcall(function()
             game:GetService("StarterGui"):SetCore("SendNotification", {
                 Title = "Script Not Found",
