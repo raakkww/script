9a19a50323640c0c4f35b5ad5bbdbb33176c245d
raakkww <aditiapribadiansyah5@gmail.com>
2026-03-01 04:08:22 +0700

    Update loaderdev.lua

--- loaderdev.lua
+++ loaderdev.lua
@@ -9,23 +9,6 @@ task.spawn(function()
     end)
 
     if not ok then
-        warn("Error: " .. tostring(err))
-
-        local universeId = tostring(game.GameId):gsub("%s+", "")
-        warn("Buat file: " .. universeId)
-
-        pcall(function()
-            setclipboard(universeId)
-            print("✅ Universe ID copied: " .. universeId)
-        end)
-
-        pcall(function()
-            game:GetService("StarterGui"):SetCore("SendNotification", {
-                Title = "Script Not Found",
-                Text = "Universe ID copied to clipboard!\n" .. universeId,
-                Duration = 10,
-                Button1 = "OK"
-            })
-        end)
+        warn("Error detail: " .. tostring(err))  -- lihat error aslinya
     end
 end)
